a958e13bcd8d6bf2e76af7f2e6fcb02cc946c625
Bogdan Poplauschi <bpoplauschi@gmail.com>
2022-01-12 15:24:17 +0200

    Changed the Fastis path to . (root folder) to match the current folder structure

--- Fastis/Package.swift
+++ Fastis/Package.swift
@@ -41,7 +41,7 @@ let package = Package(
         .target(
             name: "Fastis",
             dependencies: ["JTAppleCalendar", "SnapKit", "PrettyCards"],
-            path: "./Sources",
+            path: ".",
             sources: ["Resources", "Sources", "Derived/Sources"]
         )
     ],
